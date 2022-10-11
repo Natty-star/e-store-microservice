63ec4903c722545499c8a905a455a99746fac459
Natty Teshome <nattyteshome30@gmail.com>
2022-10-11 09:24:41 -0500

    initial commit

---
+++ payPal-service/src/test/java/edu/miu/cs590/payPalservice/PayPalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs590.payPalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayPalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ service-registry/src/test/java/edu/miu/cs590/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs590.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
