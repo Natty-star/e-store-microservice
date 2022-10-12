3a2c953653b6b57a5436d1206be007019b7af9f7
Dawit Meketa <meketadawit@gmail.com>
2022-10-12 16:09:54 -0500

    Revert "Payment : Credit card : Paypal : Bank"
    This reverts commit b7e5e1841c0744dc2bc821a209bbe214b15b8522.

---
+++ bankAccount-service/src/main/java/edu/miu/cs590/bankAccountservice/BankAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs590.bankAccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountServiceApplication.class, args);
+	}
+
+}
